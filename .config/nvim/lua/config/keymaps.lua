e14e42ded058afca5906799f9fa0b12589ecb680
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-02-04 19:15:59 +0300

    Add telescope keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg("v")
+  vim.fn.setreg("v", {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ""
+  end
+end
+
 local discipline = require("akoken.discipline")
 discipline.cowboy()
 
@@ -45,4 +58,18 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
+end, opts)
+
+local tb = require("telescope.builtin")
+
+keymap.set("n", "<space>g", ":Telescope current_buffer_fuzzy_find<cr>", opts)
+keymap.set("v", "<space>g", function()
+  local text = vim.getVisualSelection()
+  tb.current_buffer_fuzzy_find({ default_text = text })
+end, opts)
+
+keymap.set("n", "<space>G", ":Telescope live_grep<cr>", opts)
+keymap.set("v", "<space>G", function()
+  local text = vim.getVisualSelection()
+  tb.live_grep({ default_text = text })
 end, opts)
