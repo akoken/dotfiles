e923494a174d7097070ed93a00e423bb58442c27
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-02-21 09:17:15 +0300

    Add smoot scrolling improvement

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -10,6 +10,19 @@ return {
     end,
   },
 
+  -- Smooth scrolling neovim plugin written in lua
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({
+        stop_eof = true,
+        easing_function = "sine",
+        hide_cursor = true,
+        cursor_scrolls_alone = true,
+      })
+    end,
+  },
+
   -- buffer line
   {
     "akinsho/bufferline.nvim",
