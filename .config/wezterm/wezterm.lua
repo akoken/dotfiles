c5e16ee6b81fb55bef5cee943b169561f0f6fea4
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-08-22 22:08:48 +0300

    Clean up wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,12 +15,9 @@ local wallpapers_glob = "/Users/akoken/Documents/Wallpapers/active/**"
 
 local b = require("utils/background")
 local cs = require("utils/color_scheme")
-local h = require("utils/helpers")
-local k = require("utils/keys")
 local w = require("utils/wallpaper")
 
 local wezterm = require("wezterm")
-local act = wezterm.action
 
 local config = {
 	background = {
@@ -31,6 +28,7 @@ local config = {
 	line_height = 1.1,
 	font_size = 20,
 	font = wezterm.font_with_fallback({
+		"Liga SFMono Nerd Font",
 		"Monaspace Neon",
 		"CaskaydiaCove Nerd Font Mono",
 	}),
@@ -45,7 +43,7 @@ local config = {
 	},
 
 	set_environment_variables = {
-		BAT_THEME = h.is_dark() and "Catppuccin Mocha" or "Catppuccin Latte",
+		BAT_THEME = cs.get_color_scheme(),
 		LC_ALL = "en_US.UTF-8",
 	},
 
@@ -60,104 +58,6 @@ local config = {
 
 	-- keys
 	keys = {
-		k.cmd_key(".", k.multiple_actions(":ZenMode")),
-		k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
-		k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
-		k.cmd_key("f", k.multiple_actions(":Grep")),
-		k.cmd_key("i", k.multiple_actions(":SmartGoTo")),
-		k.cmd_key("O", k.multiple_actions(":GoToSymbol")),
-		k.cmd_key("P", k.multiple_actions(":GoToCommand")),
-		k.cmd_key("p", k.multiple_actions(":GoToFile")),
-		k.cmd_key("q", k.multiple_actions(":qa!")),
-		k.cmd_to_tmux_prefix("1", "1"),
-		k.cmd_to_tmux_prefix("2", "2"),
-		k.cmd_to_tmux_prefix("3", "3"),
-		k.cmd_to_tmux_prefix("4", "4"),
-		k.cmd_to_tmux_prefix("5", "5"),
-		k.cmd_to_tmux_prefix("6", "6"),
-		k.cmd_to_tmux_prefix("7", "7"),
-		k.cmd_to_tmux_prefix("8", "8"),
-		k.cmd_to_tmux_prefix("9", "9"),
-		k.cmd_to_tmux_prefix("`", "n"),
-		k.cmd_to_tmux_prefix("b", "b"),
-		k.cmd_to_tmux_prefix("C", "C"),
-		k.cmd_to_tmux_prefix("d", "D"),
-		k.cmd_to_tmux_prefix("G", "G"),
-		k.cmd_to_tmux_prefix("g", "g"),
-		k.cmd_to_tmux_prefix("j", "J"),
-		k.cmd_to_tmux_prefix("K", "T"),
-		k.cmd_to_tmux_prefix("k", "K"),
-		k.cmd_to_tmux_prefix("l", "L"),
-		k.cmd_to_tmux_prefix("n", '"'),
-		k.cmd_to_tmux_prefix("N", "%"),
-		k.cmd_to_tmux_prefix("o", "u"),
-		k.cmd_to_tmux_prefix("T", "!"),
-		k.cmd_to_tmux_prefix("Y", "Y"),
-		k.cmd_to_tmux_prefix("t", "c"),
-		k.cmd_to_tmux_prefix("w", "x"),
-		k.cmd_to_tmux_prefix("z", "z"),
-		k.cmd_to_tmux_prefix("Z", "Z"),
-
-		k.cmd_key(
-			"R",
-			act.Multiple({
-				act.SendKey({ key = "\x1b" }), -- escape
-				k.multiple_actions(":source %"),
-			})
-		),
-
-		k.cmd_key(
-			"s",
-			act.Multiple({
-				act.SendKey({ key = "\x1b" }), -- escape
-				k.multiple_actions(":w"),
-			})
-		),
-
-		{
-			mods = "CMD|SHIFT",
-			key = "}",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "n" }),
-			}),
-		},
-		{
-			mods = "CMD|SHIFT",
-			key = "{",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "p" }),
-			}),
-		},
-
-		{
-			mods = "CTRL",
-			key = "Tab",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "n" }),
-			}),
-		},
-
-		{
-			mods = "CTRL|SHIFT",
-			key = "Tab",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "n" }),
-			}),
-		},
-
-		{
-			mods = "CMD",
-			key = "~",
-			action = act.Multiple({
-				act.SendKey({ mods = "CTRL", key = "b" }),
-				act.SendKey({ key = "p" }),
-			}),
-		},
-
 		-- Sends ESC + b and ESC + f sequence, which is used
 		-- for telling your shell to jump back/forward.
 		{
@@ -176,76 +76,5 @@ local config = {
 		},
 	},
 }
-
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	wezterm.log_info("name", name)
-	wezterm.log_info("value", value)
-
-	if name == "T_SESSION" then
-		local session = value
-		wezterm.log_info("is session", session)
-		overrides.background = {
-			w.set_tmux_session_wallpaper(value),
-			{
-				source = {
-					Gradient = {
-						colors = { "#000000" },
-					},
-				},
-				width = "100%",
-				height = "100%",
-				opacity = 0.95,
-			},
-		}
-	end
-
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-		else
-			overrides.font_size = number_value
-		end
-	end
-	if name == "DIFF_VIEW" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.DecreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			-- overrides.background = {
-			-- 	w.set_nvim_wallpaper("Diffview.jpeg"),
-			--
-			-- 	{
-			-- 		source = {
-			-- 			Gradient = {
-			-- 				colors = { "#000000" },
-			-- 			},
-			-- 		},
-			-- 		width = "100%",
-			-- 		height = "100%",
-			-- 		opacity = 0.95,
-			-- 	},
-			-- }
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.background = nil
-			overrides.font_size = nil
-		else
-			overrides.font_size = number_value
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
 
 return config
