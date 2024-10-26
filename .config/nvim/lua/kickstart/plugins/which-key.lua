3b1795140771cf96b7799e8d6e30fbf63f041402
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-10-26 12:31:36 +0300

    Fix which-key conflicts

--- .config/nvim/lua/kickstart/plugins/which-key.lua
+++ .config/nvim/lua/kickstart/plugins/which-key.lua
@@ -14,21 +14,44 @@
 --  config = function() ... end
 
 return {
-  { -- Useful plugin to show you pending keybinds.
+  --    { -- Useful plugin to show you pending keybinds.
+  --     'folke/which-key.nvim',
+  --    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  --    config = function() -- This is the function that runs, AFTER loading
+  --     require('which-key').setup()
+  --
+  --      -- Document existing key chains
+  --      require('which-key').register {
+  --        { '<leader>c', group = '[C]ode' },
+  --        { '<leader>c_', hidden = true },
+  --        { '<leader>d', group = '[D]ocument' },
+  --        { '<leader>d_', hidden = true },
+  --        { '<leader>r', group = '[R]ename' },
+  --        { '<leader>r_', hidden = true },
+  --        { '<leader>s', group = '[S]earch' },
+  --        { '<leader>s_', hidden = true },
+  --        { '<leader>w', group = '[W]orkspace' },
+  --       { '<leader>w_', hidden = true },
+  --      }
+  --    end,
+  --  },
+  {
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-    end,
+    event = 'VeryLazy',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
