b4985aecb090a35b5bd38bccc968c80c5ad28f47
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2024-11-23 16:58:08 +0300

    Disable some MacOS settings

--- install.sh
+++ install.sh
@@ -272,8 +272,8 @@ setup_macos() {
     echo "Enable subpixel font rendering on non-Apple LCDs"
     defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
-    echo "Use current directory as default search scope in Finder"
-    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    #echo "Use current directory as default search scope in Finder"
+    #defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
     echo "Show Path bar in Finder"
     defaults write com.apple.finder ShowPathbar -bool true
@@ -281,17 +281,17 @@ setup_macos() {
     echo "Show Status bar in Finder"
     defaults write com.apple.finder ShowStatusBar -bool true
 
-    echo "Disable press-and-hold for keys in favor of key repeat"
-    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+    #echo "Disable press-and-hold for keys in favor of key repeat"
+    #defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-    echo "Set a blazingly fast keyboard repeat rate"
-    defaults write NSGlobalDomain KeyRepeat -int 1
+    #echo "Set a blazingly fast keyboard repeat rate"
+    #defaults write NSGlobalDomain KeyRepeat -int 1
 
-    echo "Set a shorter Delay until key repeat"
-    defaults write NSGlobalDomain InitialKeyRepeat -int 15
+    #echo "Set a shorter Delay until key repeat"
+    #defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-    echo "Enable tap to click (Trackpad)"
-    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+    #echo "Enable tap to click (Trackpad)"
+    #defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 
     echo "Enable Safari’s debug menu"
     defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
