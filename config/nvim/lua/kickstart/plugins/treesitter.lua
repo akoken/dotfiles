72ac7689003dd2c030e5be5e8324bfdf7613c88c
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2025-01-12 22:43:55 +0300

    Add C# linter

--- config/nvim/lua/kickstart/plugins/treesitter.lua
+++ config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'rust', 'toml', 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'rust', 'toml', 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'c_sharp' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
