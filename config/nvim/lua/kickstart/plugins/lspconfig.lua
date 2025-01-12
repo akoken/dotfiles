a65e188b86f69eaa481f20244710a8666d16f7c3
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2025-01-12 22:43:29 +0300

    Add C# language server

--- config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -169,6 +169,7 @@ return {
             },
           },
         },
+        omnisharp = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -184,6 +185,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'csharpier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
