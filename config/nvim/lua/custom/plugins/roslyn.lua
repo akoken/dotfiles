b55cf18061e3ca35b96fa88b55d5287beeaa1a06
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2025-09-08 14:29:26 +0300

    Add roslyn plugin

---
+++ config/nvim/lua/custom/plugins/roslyn.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'seblyng/roslyn.nvim',
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    ft = { 'cs', 'razor' },
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
+}
