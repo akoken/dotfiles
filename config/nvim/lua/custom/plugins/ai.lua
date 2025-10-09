80d1291b0bd7b96f3ba766ee7f9533a3f82ec6c6
Abdurrahman Alp Köken <abdurrahman@akoken.com>
2025-10-09 23:45:03 +0300

    Add nvim code companion plugin

---
+++ config/nvim/lua/custom/plugins/ai.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'olimorris/codecompanion.nvim',
+    opts = {},
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'ravitemer/mcphub.nvim',
+    },
+  },
+  {
+    'echasnovski/mini.diff',
+    config = function()
+      local diff = require 'mini.diff'
+      diff.setup {
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      }
+    end,
+  },
+  {
+    'HakonHarnes/img-clip.nvim',
+    opts = {
+      filetypes = {
+        codecompanion = {
+          prompt_for_file_name = false,
+          template = '[Image]($FILE_PATH)',
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
